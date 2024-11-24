63543e93511c0d571303d954f24ce5055bdd5eab
LioznTheBun <etienne.pourchon@gmail.com>
2024-11-24 12:39:49 +0100

    Initialisation du porfolio

---
+++ projets.php
@@ -0,0 +1,101 @@
+<?php include("parties/header.html") ?>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="assets/css/projets.css" media="all" />
+</head>
+<article id="projets" class="section">
+    <div class="container">
+        <h2 class="titre">Mes stages</h2>
+        <div class="row">
+            <section class="col-md-11 col-lg-5">
+                <h4>Stage 1ère année</h4>
+                <div class="project">
+                    <img src="assets/img/stage.svg" alt="Stage première année" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="assets/docs/Rapport de Stage 1ère année Etienne POURCHON.pdf" target="_blank" class="btn btn-primary">Rapport en PDF</a>
+                        <a href="assets/docs/Attestation_stage_Etienne_Pourchon .pdf" target="_blank" class="btn btn-secondary">Attestation en PDF</a>
+                    </div>
+                </div>
+            </section>
+            <section class="col-md-11 col-lg-5">
+                <h4>Stage 2ème année</h4>
+                <div class="project">
+                    <img src="assets/img/stage.svg" alt="Stage deuxième année" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="assets/docs/Rapport de Stage 2ème année Etienne Pourchon .pdf" target="_blank" class="btn btn-primary">Rapport en PDF</a>
+                        <a href="assets/docs/Attestation_stage_2_Etienne_Pourchon.pdf" target="_blank" class="btn btn-secondary">Attestation en PDF</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</article>
+<article class="section">
+    <div class="container">
+        <h2 class="titre">Mes projets</h2>
+        <div class="row">
+            <section class="col-md-11 col-lg-5">
+                <h4>Fansite</h4>
+                <div class="project">
+                    <img src="assets/img/Fansite.svg" alt="Project 1 : Fansite" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="project1_description.pdf" target="_blank" class="col-7 btn btn-primary">Voir Documentation</a>
+                        <a href="Fansite/index.html" target="_blank" class="col-7 btn btn-secondary">Lien Demo "Fansite"</a>
+                    </div>
+                </div>
+            </section>
+            <section class="col-md-11 col-lg-5">
+                <h4>Forum</h4>
+                <div class="project">
+                    <img src="assets/img/Forum.svg" alt="Project 2 : Forum" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="project2_description.pdf" target="_blank" class="btn btn-primary">Voir Documentation</a>
+                        <a href="https://github.com/LioznTheBun/Project_forum" target="_blank" class="btn btn-secondary">Lien Git "Forum" </a>
+                    </div>
+                </div>
+            </section>
+            <section class="col-md-11 col-lg-5">
+                <h4>Site de Location //Incomplet//</h4>
+                <div class="project">
+                    <img src="assets/img/Logement.svg" alt="Project 3 : Location" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="project2_description.pdf" target="_blank" class="btn btn-primary">Voir Documentation</a>
+                        <a href="https://github.com/LioznTheBun/Projet-Location" target="_blank" class="btn btn-secondary">Lien Git "Location" </a>
+                    </div>
+                </div>
+            </section>
+            <section class="col-md-11 col-lg-5">
+                <h4>Borne Restaurant de Tapas</h4>
+                <div class="project">
+                    <img src="assets/img/Tapas.svg" alt="Project 4 : Borne Tapas" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="project2_description.pdf" target="_blank" class="btn btn-primary">Voir Documentation</a>
+                        <a href="https://github.com/LioznTheBun/Projet-Tapas" target="_blank" class="btn btn-secondary">Lien Git "Borne Tapas" </a>
+                    </div>
+                </div>
+            </section>
+            <section class="col-md-11 col-lg-5">
+                <h4>Instagrume</h4>
+                <div class="project">
+                    <img src="assets/img/Instagrume.svg" alt="Project 5 : Instagrume" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="assets/docs/Rapport Instagrume.pdf" target="_blank" class="btn btn-primary">Voir Documentation</a>
+                        <a href="https://github.com/LioznTheBun/instagrume_groupe3_api" target="_blank" class="btn btn-secondary">Lien Git API</a>
+                        <a href="https://github.com/LioznTheBun/instagrume_groupe3_client" target="_blank" class="btn btn-secondary">Lien Git Client </a>
+                    </div>
+                </div>
+            </section>
+            <section class="col-md-11 col-lg-5">
+                <h4>App mobile liste de course</h4>
+                <div class="project">
+                    <img src="assets/img/android.svg" alt="Project 6 : App mobile" class="img-fluid">
+                    <div class="row buttons">
+                        <a href="assets/docs/Rapport App Android.pdf" target="_blank" class="btn btn-primary">Voir Documentation</a>
+                        <a href="https://github.com/LioznTheBun/App_Liste_Courses" target="_blank" class="btn btn-secondary">Lien Git "App mobile" </a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</article>
+<?php include("parties/footer.html") ?>
---
+++ veille.php
@@ -0,0 +1,29 @@
+<?php include("parties/header.html") ?>
+<link rel="stylesheet" type="text/css" href="assets/css/veille.css" media="all" />
+
+<section id="articles">
+        <h2>Veille</h2>
+        <div class="article">
+            <a href="https://hal.science" target="_blank"><img src="assets/img/preview-1.PNG" alt="Article 1 Image" class="article-image"></a>
+            <div class="article-content">
+                <h3>« Comment l’intelligence artificielle peut révolutionner les techniques de test de logiciels » </h3>
+                <p>de Moez Krichen paru en 2023 sur le site HAL open science. <a href="https://hal.science/hal-04054712/document" target="_blank">Lire l'article</a></p>
+            </div>
+        </div>
+        <div class="article">
+            <a href="https://www.lemondeinformatique.fr" target="_blank"><img src="assets/img/preview-2.jpg" alt="Article 2 Image" class="article-image"></a>
+            <div class="article-content">
+                <h3>« Comment améliorer les tests continus à l’heure de l’IA générative »</h3>
+                <p>de Isaac Sacolick publié le 1er septembre 2023 sur le site Le Monde de l’informatique. <a href="https://www.lemondeinformatique.fr/actualites/lire-comment-ameliorer-les-tests-continus-a-l-heure-de-l-ia-generative-91322.html" target="_blank">Lire l'article</a></p>
+            </div>
+        </div>
+        <div class="article">
+            <a href="https://www.codecademy.com" target="_blank"><img src="assets/img/preview-3.png" alt="Article 3 Image" class="article-image"></a>
+            <div class="article-content">
+                <h3>« La différence entre IA, Machine learning, & Deep Learning - Expliqué »</h3>
+                <p>de Cory Stieg publié le 24 juillet 2023 sur le site Codecademy (Article en anglais)<a href="https://www.codecademy.com/resources/blog/difference-between-ai-machine-deep-learning-examples/" target="_blank">Lire l'article</a></p>
+            </div>
+        </div>
+    </section>
+
+<?php include("parties/footer.html") ?>
